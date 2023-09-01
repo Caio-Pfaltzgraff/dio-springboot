8c41708fb86a731028a9f6883171e2376ec57710
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-09-01 11:55:48 -0300

    Query methods e Query Overrides

--- spring-data-jpa/src/main/java/dio/springboot/StartApp.java
+++ spring-data-jpa/src/main/java/dio/springboot/StartApp.java
@@ -6,13 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class StartApp implements CommandLineRunner {
     @Autowired
     private UserRepository repository;
     @Override
     public void run(String... args) throws Exception {
-        repository.save(new User(null, "Ruan", "rubrabo16", "4321"));
+        List<User> users = repository.filtrarPorNome("Caio");
+        for (User u : users) {
+            System.out.println(u);
+        }
+    }
+
+    private void insertUser() {
+        repository.save(new User(null, "Caio Pfaltzgraff", "kaio0416", "2468"));
 
         for (User u: repository.findAll()) {
             System.out.println(u);
--- spring-data-jpa/src/main/java/dio/springboot/repository/UserRepository.java
+++ spring-data-jpa/src/main/java/dio/springboot/repository/UserRepository.java
@@ -2,8 +2,17 @@ package dio.springboot.repository;
 
 import dio.springboot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    //Query Method
+    User findByUsername(String username);
+
+    //Query Override
+    @Query("SELECT u FROM User u WHERE u.name LIKE %:name%")
+    List<User> filtrarPorNome(String name);
 }
